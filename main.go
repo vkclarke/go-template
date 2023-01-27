acab4a86dc8917dfac8d3b83622a2a280c7f3ad4
Vincent Clarke <vincent@windows97.space>
2023-01-26 20:53:03 -0500

    we can just do this instead

--- main.go
+++ main.go
@@ -9,10 +9,7 @@ import (
 func init() {
 	log.SetFlags(0)
 	name := os.Args[0]
-	if i := strings.LastIndexAny(name, "/\\"); i > -1 {
-		name = name[i+1:]
-	}
-	log.SetPrefix(name + ": ")
+	log.SetPrefix(name[strings.LastIndexAny(name, "/\\")+1:] + ": ")
 }
 
 func main() {
